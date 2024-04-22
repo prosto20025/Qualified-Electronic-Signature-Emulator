5f58dd07b1e82bda056ed79de0bf5d32bfa916ea
Filip <fifikrawczak@interia.pl>
2024-04-22 09:54:53 +0200

    New key generation and new window interface

--- GUI/main.py
+++ GUI/main.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from tkinter import filedialog
+import psutil
+import os
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import AES, PKCS1_OAEP
+from hashlib import sha256
+from Crypto.Util.number import bytes_to_long
 
 # Create main Tkinter window
 root = tk.Tk()
@@ -10,19 +16,57 @@ root.title("Document Signing and Encryption")
 frame = tk.Frame(root)
 frame.pack(anchor='center')
 
+# Function to check if a pendrive is connected
+def check_pendrive():
+    key_file_found = False
+    for device in psutil.disk_partitions():
+        if 'removable' in device.opts:
+            pendrive_path = device.mountpoint
+            for file in os.listdir(pendrive_path):
+                if file.endswith(".key"):
+                    key_file_found = True
+                    break
+            break
+    sign_button.config(state=tk.NORMAL if key_file_found else tk.DISABLED)
+    # Schedule the function to run again after 1 second
+    root.after(1000, check_pendrive)
+
+
 # Define a function to open a file dialog
 def open_file_dialog(callback):
     file_path = filedialog.askopenfilename()
     print(f"Selected file: {file_path}")
     callback(file_path)
 
+
 def open_file(entry):
     file_path = filedialog.askopenfilename()
     entry.delete(0, tk.END)
     entry.insert(0, file_path)
 
-# Define a function to close the current window and open a new one
-def open_new_window():
+
+def open_new_window_sign_document():
+    root.destroy()
+    new_window = tk.Tk()
+    new_window.geometry("500x300")
+    new_window.title("Sign Document")
+
+    detected_label = tk.Label(new_window, text="Detected USB with key file")
+    detected_label.pack(pady=10)
+
+    pin_label = tk.Label(new_window, text="Enter your PIN:")
+    pin_label.pack(pady=10)
+
+    pin_entry = tk.Entry(new_window, show="*")
+    pin_entry.pack(pady=5)
+
+    sign_button = tk.Button(new_window, text="Enter PIN", command=lambda: sign_document(pin_entry.get()))
+    sign_button.pack(pady=10)
+
+    new_window.mainloop()
+
+
+def open_new_window_verify_signature():
     root.destroy()
     new_window = tk.Tk()
     new_window.geometry("500x300")
@@ -52,19 +96,29 @@ def open_new_window():
 
     new_window.mainloop()
 
+
 def encrypt(file_path):
+    
     print(f"Encrypting file: {file_path}")
+
+def sign_document(file_path):
+    print(f"Signing file: {file_path}")
+
+
 
 def decrypt(file_path):
     print(f"Decrypting file: {file_path}")
+
 
 def verify(key_path, xml_path):
     print(f"Verifying with key: {key_path} and xml: {xml_path}")
 
 
+
+
 # Create buttons
-sign_button = tk.Button(frame, text="Sign document")
-verify_button = tk.Button(frame, text="Verify signature", command=open_new_window)
+sign_button = tk.Button(frame, text="Sign document", state=tk.DISABLED, command=open_new_window_sign_document)
+verify_button = tk.Button(frame, text="Verify signature", command=open_new_window_verify_signature)
 encrypt_button = tk.Button(frame, text="Encrypt", command=lambda: open_file_dialog(encrypt))
 decrypt_button = tk.Button(frame, text="Decrypt", command=lambda: open_file_dialog(decrypt))
 
@@ -73,6 +127,9 @@ sign_button.pack(pady=10)
 verify_button.pack(pady=10)
 encrypt_button.pack(pady=10)
 decrypt_button.pack(pady=10)
+
+# Run the function to check if a pendrive is connected
+check_pendrive()
 
 # Run the Tkinter event loop
 root.mainloop()
--- Key_Generator/main.py
+++ Key_Generator/main.py
@@ -1,37 +1,29 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
 from hashlib import sha256
 
-# Generate RSA key pair
-key = RSA.generate(4096)
+key_length = 4096
+user_pin = "2134"
+cipher_block_size = AES.block_size
+cipher_mode = AES.MODE_CBC
+
+rng = get_random_bytes
+
+key = RSA.generate(key_length, rng)
 private_key = key.export_key()
 public_key = key.publickey().export_key()
 
-# User A's PIN number
-pin = "2134"
+pin_hash = sha256(user_pin.encode()).digest()
+cipher = AES.new(pin_hash, mode=cipher_mode)
+encrypted_private_key = cipher.encrypt(pad(private_key, cipher_block_size))
 
-# Hash the PIN using SHA-256
-pin_hash = sha256(pin.encode()).digest()
-
-# Encrypt the private key with AES using the hashed PIN as the key
-cipher = AES.new(pin_hash, AES.MODE_EAX)
-ciphertext = cipher.encrypt(private_key)
-
-# Save the public key
 print('Public key')
-with open('keys/public_key.key', 'wb') as file:
+with open('../keys/public_key.key', 'wb') as file:
     file.write(public_key)
-
-# Wrap the AES key with the user's public RSA key
-rsa_key = RSA.import_key(public_key)
-cipher_rsa = PKCS1_OAEP.new(rsa_key)
-encrypted_aes_key = cipher_rsa.encrypt(pin_hash)
-
-# Store the encrypted private key and encrypted AES key on the pendrive
-encrypted_private_key = ciphertext + encrypted_aes_key
 
 # Save the encrypted private key and encrypted AES key
 print('Encrypted private key')
-with open('keys/private_key.key', 'wb') as file:
+with open('../keys/private_key.key', 'wb') as file:
     file.write(encrypted_private_key)
